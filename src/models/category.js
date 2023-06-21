1d446326aec1552f52d95df1c41c144cc4a79a9f
ThiagoThe <nitrox2thi@gmail.com>
2023-06-21 20:51:34 -0300

    feat: instalado dependencia dotenv

---
+++ src/models/category.js
@@ -0,0 +1,3 @@
+/*  npmx sequelize-cli model:generate --name Trainee --attributes name:string, created_at:date, update_at:date */
+/* npx sequelize-cli migration:generate --name nomequalquer */
+/*npx sequelize db:migrate*/
