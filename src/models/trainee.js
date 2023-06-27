d2cad843eac7cb2d7840c5c08edadd1c254418c1
ThiagoThe <nitrox2thi@gmail.com>
2023-06-27 20:50:04 -0300

    refactor: trainee atualizado

--- src/models/trainee.js
+++ src/models/trainee.js
@@ -1,19 +1,54 @@
 const { connection } = require("../database/connection");
 const { STRING, DATE, BOOLEAN } = require("sequelize");
 
-const trainee = connection.define("trainee", {
-  name: STRING,
-  created_at: DATE,
-  updated_at: DATE,
-  email: STRING,
-  rg: STRING,
-  cpf: STRING,
-  primary_phone_contact: STRING,
-  secondary_phone_contact: STRING,
-  date_birth: DATE,
-  father_name: STRING,
-  mother_name: STRING,
-  have_special_needs: BOOLEAN,
-});
+const trainee = connection.define(
+  "trainee",
+  {
+    name: STRING,
+    created_at: DATE,
+    updated_at: DATE,
+    email: {
+      validate: {
+        isEmail: {
+          msg: "O campo enviado deve ser um e-mail válido!",
+        },
+      },
+    },
+    rg: {
+      type: STRING,
+      validate: {
+        min: {
+          args: 7,
+          msg: "O campo RG deve ter no mínimo 7 caracteres!",
+        },
+      },
+      unique: {
+        msg: "Este campo já está na aplicação",
+      },
+    },
+    cpf: {
+      type: STRING,
+      validate: {
+        len: {
+          args: [11, 11],
+          msg: "O campo CPF deve ter exatamente 11 caracteres!",
+        },
+      },
+      unique: {
+        msg: "Este campo já está na aplicação",
+      },
+    },
+    primary_phone_contact: STRING,
+    secondary_phone_contact: {
+      type: STRING,
+      allowNull: true,
+    },
+    date_birth: DATE,
+    father_name: STRING,
+    mother_name: STRING,
+    have_special_needs: BOOLEAN,
+  },
+  { underscored: true }
+);
 
-module.exports = trainee;
+module.exports = { trainee };
